e1c1e1fde24a3e098ca22d3deb77d6d42deafcc1
Emiel van Goor <emielvangoor@me.com>
2019-01-21 14:26:38 +0100

    Initial setup

---
+++ src/Collection.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * EXB R5 - Business suite
+ * Copyright (C) EXB Software 2018 - All Rights Reserved
+ *
+ * This file is part of EXB R5.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * 2019-01-21
+ *
+ * @author emiel <e.goor@exb-software.com>
+ */
+declare(strict_types=1);
+
+namespace EXB\Component;
+
+class Collection
+{
+
+}
